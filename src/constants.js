028c8bca94bbce1133613d1dc283e34735744294
pterror <pterrorbird@gmail.com>
2025-12-24 15:42:27 +1000

    change filters from OFF to BLOCK_NONE

--- src/constants.js
+++ src/constants.js
@@ -141,46 +141,46 @@ export const UNSAFE_EXTENSIONS = [
 export const GEMINI_SAFETY = [
     {
         category: 'HARM_CATEGORY_HARASSMENT',
-        threshold: 'OFF',
+        threshold: 'BLOCK_NONE',
     },
     {
         category: 'HARM_CATEGORY_HATE_SPEECH',
-        threshold: 'OFF',
+        threshold: 'BLOCK_NONE',
     },
     {
         category: 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
-        threshold: 'OFF',
+        threshold: 'BLOCK_NONE',
     },
     {
         category: 'HARM_CATEGORY_DANGEROUS_CONTENT',
-        threshold: 'OFF',
+        threshold: 'BLOCK_NONE',
     },
     {
         category: 'HARM_CATEGORY_CIVIC_INTEGRITY',
-        threshold: 'OFF',
+        threshold: 'BLOCK_NONE',
     },
 ];
 
 export const VERTEX_SAFETY = [
     {
         category: 'HARM_CATEGORY_IMAGE_HATE',
-        threshold: 'OFF',
+        threshold: 'BLOCK_NONE',
     },
     {
         category: 'HARM_CATEGORY_IMAGE_DANGEROUS_CONTENT',
-        threshold: 'OFF',
+        threshold: 'BLOCK_NONE',
     },
     {
         category: 'HARM_CATEGORY_IMAGE_HARASSMENT',
-        threshold: 'OFF',
+        threshold: 'BLOCK_NONE',
     },
     {
         category: 'HARM_CATEGORY_IMAGE_SEXUALLY_EXPLICIT',
-        threshold: 'OFF',
+        threshold: 'BLOCK_NONE',
     },
     {
         category: 'HARM_CATEGORY_JAILBREAK',
-        threshold: 'OFF',
+        threshold: 'BLOCK_NONE',
     },
 ];
 
